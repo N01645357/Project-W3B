cf5fe0189cf45522698c1306bce2c5845c934db1
aaronotacku <147105125+aaronotacku@users.noreply.github.com>
2023-12-07 19:34:43 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ function scrollToTop() {
 function scrollToBottom() {
     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
 }
+
+function footerHide() {
+    var footer = document.getElementById("mainFooter");
+    if (footer.style.display === "none") {
+        footer.style.display = "block";
+    } else {
+        footer.style.display = "none";
+    }
+}
+function footerShow() {
+    var footer = document.getElementById("mainFooter");
+    footer.style.display = "block";
+}
