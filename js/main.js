c3367f428ebeea87158feb3504e0929bdc2a0ecc
aaronotacku <aalaoshe@gmail.com>
2023-12-08 20:33:28 -0500

    -comments to js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
+// Button to quick scroll to the top of the page
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
-
+// Button to quick scroll to the bottom of the page
 function scrollToBottom() {
     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
 }
 
+// Button to hide the footer
 function footerHide() {
     var footer = document.getElementById("mainFooter");
     footer.style.display = "none";
 }
-
+// Button to show the footer when hidden
 function footerShow() {
     var footer = document.getElementById("mainFooter");
     footer.style.display = "block";
